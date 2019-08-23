689b27cf4f18b6ec34d015c60942febb03f38fe6
Jing Kang <ngjingkang97@gmail.com>
2019-08-23 12:08:19 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -9,18 +9,31 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         Scanner scan = new Scanner(System.in);
+        String[] todo = new String [100];
+        int index = 0;
         while(true){
-            String input = scan.next();
+            String input = scan.nextLine();
+
             if(input.equals("bye")){
                 System.out.println("______________________________________________________________________________");
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println("______________________________________________________________________________");
                 break;
             }
-            System.out.println("______________________________________________________________________________");
-            System.out.println(input);
-            System.out.println("______________________________________________________________________________");
+            if(input.equals("list")){
+                for(int i = 0; i < todo.length; i += 1){
+                    if(todo[i]!=null){
+                        System.out.println(i+1 + ". " + todo[i]);
+                    }
+                }
+            }
+            else{
+                todo[index] = input;
+                index += 1;
+                System.out.println("______________________________________________________________________________");
+                System.out.println("added: " + input);
+                System.out.println("______________________________________________________________________________");
+            }
         }
-
     }
 }
