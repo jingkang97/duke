472792c47c72b4d073536ed24ae6975b8ab9d265
Jing Kang <ngjingkang97@gmail.com>
2019-08-28 12:21:38 +0800

    Level-8

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,22 +1,56 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
     protected String by;
-
+    protected String DeadlineFormatDateTime;
     public Deadline(String description, String by) throws DukeException{
         super(description);
         if(description.equals("deadline ") || description.equals("deadline")){
             throw new DukeException("______________________________________________________________________________\n ☹ OOPS!!! The description of a deadline cannot be empty.\n______________________________________________________________________________");
         }
-        if(by.equals(description) || by.equals(" ")){
+        if(by.equals(description.trim()) || by.equals(" ")){
             throw new DukeException("______________________________________________________________________________\n ☹ OOPS!!! The time and date of a deadline cannot be empty.\n______________________________________________________________________________");
         }
         else{
-            this.by = by;
+            try {
+                by.trim();
+                this.by = by;
+                DateTimeFormat(this.by);
+                this.DeadlineFormatDateTime = DateTimeFormat(this.by);
+            }catch(Exception e){
+                throw new DukeException("Please input the date and time in this format: dd/mm/yyyy hhmm");
+            }
         }
 
     }
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by:" + by + ")";//super will use the Parent's/base class method instead of the child's/sub class method
+        return "[D]" + super.toString() + "(by: " + DeadlineFormatDateTime + ")";//super will use the Parent's/base class method instead of the child's/sub class method
+    }
+
+    public String DateTimeFormat(String dateTime){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        LocalDateTime localDate = LocalDateTime.parse(dateTime, formatter);
+        String localDateTimeString = formatter.format(localDate);
+
+        DateTimeFormatter customFormatter = DateTimeFormatter.ofPattern("d'" + ordinalConversion(localDateTimeString) + "' 'of' MMMM yyyy, h':'mma");
+        String result = customFormatter.format(localDate);
+        return result;
+    }
+    public String ordinalConversion(String dateTimeFormat){
+        String customToken [] = dateTimeFormat.split("/");
+        int ordinalNumber = Integer.parseInt(customToken[0]);
+        switch(ordinalNumber % 10){
+            case 1:
+                return "st";
+            case 2:
+                return "nd";
+            case 3:
+                return "rd";
+            default:
+                return "th";
+        }
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,22 +1,56 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
 
     protected String at;
+    protected String EventFormatDateTime;
 
     public Event(String description, String at)throws DukeException{
         super(description);
-        if(description.equals("event ") || description.equals("event")){
+        if (description.equals("event ") || description.equals("event")) {
             throw new DukeException("______________________________________________________________________________\n ☹ OOPS!!! The description of an event cannot be empty.\n______________________________________________________________________________");
         }
-        if(at.equals(description) || at.equals(" ")){
+        if (at.equals(description.trim()) || at.equals(" ")) {
             throw new DukeException("______________________________________________________________________________\n ☹ OOPS!!! The time and date of an event cannot be empty.\n______________________________________________________________________________");
-        }
-        else{
-            this.at = at;
+        } else {
+
+            try {
+                at.trim();
+                this.at = at;
+                DateTimeFormat(this.at);
+                this.EventFormatDateTime = DateTimeFormat(this.at);
+            }catch(Exception e){
+                throw new DukeException("Please input the date and time in this format: dd/mm/yyyy hhmm");
+            }
         }
     }
     @Override
     public String toString(){
-        return "[E]" + super.toString() + "(at:" + at + ")";
+        return "[E]" + super.toString() + "(at: " + EventFormatDateTime + ")";
+    }
+    public String DateTimeFormat(String dateTime){
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        LocalDateTime localDate = LocalDateTime.parse(dateTime, formatter);
+        String localDateTimeString = formatter.format(localDate);
+
+        DateTimeFormatter customFormatter = DateTimeFormatter.ofPattern("d'" + ordinalConversion(localDateTimeString) + "' 'of' MMMM yyyy, h':'mma");
+        String result = customFormatter.format(localDate);
+        return result;
     }
 
+    public String ordinalConversion(String dateTimeFormat){
+        String customToken [] = dateTimeFormat.split("/");
+        int ordinalNumber = Integer.parseInt(customToken[0]);
+        switch(ordinalNumber % 10){
+            case 1:
+                return "st";
+            case 2:
+                return "nd";
+            case 3:
+                return "rd";
+            default:
+                return "th";
+        }
+    }
 }
